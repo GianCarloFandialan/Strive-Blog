1e3ec822c79f74a849c60412c46d4f66bf0fc083
Gian Carlo Fandialan <gianchifandialan03@gmail.com>
2024-07-19 16:33:47 +0200

    sistemato axios

--- frontend/src/data/apiAxios.js
+++ frontend/src/data/apiAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "https://strive-blog-p5r2.onrender.com"
+const API_URL = "https://strive-blog-p5r2.onrender.com/"
 
 const api = axios.create({ baseURL: API_URL});
 
